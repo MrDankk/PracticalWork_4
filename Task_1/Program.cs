38f974f829c25baedb991792443fcc9e1ef44ff4
Валера Безгребельный <Frostmols@gmail.com>
2024-06-04 16:44:45 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+
+            int matrixHeight, matrixWidth;
+            int matrixSum = 0;
+            int[,] matrix;
+
+            Console.WriteLine("Введите размер матрицы");
+            Console.WriteLine("Высота");
+            string inputHeight = Console.ReadLine().Trim();
+            Console.WriteLine("Ширина");
+            string inputWidth = Console.ReadLine().Trim();
+
+            //Проверка ввода
+            while (true) 
+            {
+                if (int.TryParse(inputHeight, out matrixHeight) && int.TryParse(inputWidth, out matrixWidth))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Одно из значений не является цифрой, введите значения заново");
+
+                    Console.WriteLine("Высота");
+                    inputHeight = Console.ReadLine().Trim();
+                    Console.WriteLine("Ширина");
+                    inputWidth = Console.ReadLine().Trim();
+                }
+            }
+
+            matrix = new int[matrixHeight, matrixWidth];
+
+            //Горизонтальная сумма чисел матрицы
+            for (int i = 0; i < matrixHeight; i++) 
+            {
+                int sumMatrixHorizontal = 0;
+
+                for (int j = 0; j < matrixWidth; j++)
+                {
+                    matrix[i,j] = rand.Next(101);
+                    Console.Write($"\t{matrix[i, j]}");
+
+                    sumMatrixHorizontal += matrix[i, j];
+                }
+                Console.Write($" = {sumMatrixHorizontal}");
+                matrixSum += sumMatrixHorizontal;
+                Console.WriteLine("\n");
+            }
+            
+            //Отступ с знаками равенства
+            for(int i = 0; i < matrixWidth;i++)
+            {
+                Console.Write("\t=");
+            }
+            Console.WriteLine();
+
+            //Вертикальная сумма чисел матрицы
+            for (int i = 0;i < matrixWidth;i++)
+            {
+                int sumMatrixVertical = 0;
+                for (int j = 0;j < matrixHeight;j++)
+                {
+                    sumMatrixVertical += matrix[j,i];
+                }
+                Console.Write($"\t{sumMatrixVertical}");
+            }
+            //Результат
+            Console.WriteLine($"\n\nСумма всех цисел матрицы = {matrixSum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+
+            int matrixHeight, matrixWidth;
+            int[,] firstMatrix,secondMatrix,resultMatrix;
+
+            Console.WriteLine("Введите размер матриц");
+            Console.WriteLine("Высота");
+            string inputHeight = Console.ReadLine().Trim();
+            Console.WriteLine("Ширина");
+            string inputWidth = Console.ReadLine().Trim();
+
+            //Проверка ввода
+            while (true)
+            {
+                if (int.TryParse(inputHeight, out matrixHeight) && int.TryParse(inputWidth, out matrixWidth))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Одно из значений не является цифрой, введите значения заново");
+
+                    Console.WriteLine("Высота");
+                    inputHeight = Console.ReadLine().Trim();
+                    Console.WriteLine("Ширина");
+                    inputWidth = Console.ReadLine().Trim();
+                }
+            }
+
+            //Установка размеров матриц
+            firstMatrix = new int[matrixHeight, matrixWidth];
+            secondMatrix = new int[matrixHeight, matrixWidth];
+            resultMatrix = new int[matrixHeight, matrixWidth];
+
+            //Подсчёты всех сумм и показ первой матрицы
+            Console.WriteLine("Первая матрица: \n");
+            for (int i = 0; i < matrixHeight; i++) 
+            { 
+                for (int j = 0;j < matrixWidth;j++)
+                {
+                    int firstValue = rand.Next(101);
+                    int secondValue = rand.Next(101);
+
+                    firstMatrix[i,j] = firstValue;
+                    secondMatrix[i,j] = secondValue;
+
+                    resultMatrix[i,j] = firstValue + secondValue;
+
+                    Console.Write($"\t{firstMatrix[i,j]}");
+                }
+                Console.WriteLine();
+            }
+
+            //Показ второй матрицы
+            Console.WriteLine("\nВторая матрица: \n");
+            for (int i = 0;i < matrixHeight;i++)
+            {
+                for(int j = 0; j < matrixWidth;j++)
+                {
+                    Console.Write($"\t{secondMatrix[i,j]}");
+                }
+                Console.WriteLine();
+            }
+
+            //Показ третей матрицы
+            Console.WriteLine("\nСумма двух матриц: \n");
+            for (int i = 0; i < matrixHeight; i++)
+            {
+                for (int j = 0; j < matrixWidth; j++)
+                {
+                    Console.Write($"\t{resultMatrix[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
